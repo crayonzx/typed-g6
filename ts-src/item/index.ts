ae249e7984368e1ac77fb149be96e303391574b6
Harpes <823289099@qq.com>
2019-01-15 17:01:12 +0800

    导出item全局类型

--- ts-src/item/index.ts
+++ ts-src/item/index.ts
@@ -2,15 +2,30 @@
  * @fileOverview item entry file
  * @author huangtonger@aliyun.com
  */
-
+import { Common } from '../common';
 import Node from './node';
 import Edge from './edge';
 import Group from './group';
 import Guide from './guide';
 
-export = {
+namespace Items {
+  export type Item = Node | Edge | Group | Guide;
+  export type Map<
+    T extends Common.ItemType = 'node'
+  > = T extends Common.NodeType
+    ? Node
+    : (T extends Common.EdgeType
+        ? Edge
+        : (T extends Common.GroupType
+            ? Group
+            : (T extends Common.GuideType ? Guide : never)));
+}
+
+const Items = {
   Node,
   Edge,
   Group,
   Guide,
 };
+
+export = Items;
