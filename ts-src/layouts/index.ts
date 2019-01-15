1029d53d955b8fd6d0ce58ba946b3698f93980d0
crayon <crayon.zx@gmail.com>
2019-01-15 14:11:42 +0800

    Normalize and simplify type definitions of Layouts

--- ts-src/layouts/index.ts
+++ ts-src/layouts/index.ts
@@ -3,16 +3,17 @@
  * @author huangtonger@aliyun.com
  */
 
-import CompactBoxTree from './tree/compact-box';
-import Dendrogram from './tree/dendrogram';
-import IndentedTree from './tree/indented';
-import Mindmap from './tree/mindmap';
-import Base from './base';
-
-export = {
-  CompactBoxTree: require('./tree/compact-box') as typeof CompactBoxTree,
-  Dendrogram: require('./tree/dendrogram') as typeof Dendrogram,
-  IndentedTree: require('./tree/indented') as typeof IndentedTree,
-  Mindmap: require('./tree/mindmap') as typeof Mindmap,
-  Base: require('./base') as typeof Base
+const Layouts: {
+  CompactBoxTree: typeof import('./tree/compact-box');
+  Dendrogram: typeof import('./tree/dendrogram');
+  IndentedTree: typeof import('./tree/indented');
+  Mindmap: typeof import('./tree/mindmap');
+  Base: typeof import('./base');
+} = {
+  CompactBoxTree: require('./tree/compact-box'),
+  Dendrogram: require('./tree/dendrogram'),
+  IndentedTree: require('./tree/indented'),
+  Mindmap: require('./tree/mindmap'),
+  Base: require('./base')
 };
+export = Layouts;
