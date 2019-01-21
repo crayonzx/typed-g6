16b0a12620fe57ddaaa5b8e3dd882ea359f47829
Harpes <823289099@qq.com>
2019-01-21 15:25:17 +0800

    SVGPath 从 G6 移到 G

--- ts-src/common.ts
+++ ts-src/common.ts
@@ -25,52 +25,6 @@ export namespace Common {
 
   export type Points = Array<[number, number]>;
 
-  /** https://developer.mozilla.org/zh-CN/docs/Web/SVG/Tutorial/Paths */
-  namespace SVGPath {
-    /** moveto */
-    export type M = ['M' | 'm', number, number];
-    /** lineto */
-    export type L = ['L' | 'l', number, number];
-    /** horizontal lineto */
-    export type H = ['H' | 'h'];
-    /** vertical lineto */
-    export type V = ['V' | 'v'];
-    /** curveto */
-    export type C = ['C' | 'c'];
-    /** smooth curveto */
-    export type S = ['S' | 's'];
-    /** quadratic Belzier curve */
-    export type Q = ['Q' | 'q', number, number, number, number];
-    /** smooth quadratic Belzier curveto */
-    export type T = ['T' | 't', number, number, number, number];
-    /** elliptical Arc */
-    export type A = [
-      'A' | 'a',
-      number,
-      number,
-      number,
-      number,
-      number,
-      number,
-      number
-    ];
-    /** closepath */
-    export type Z = ['Z' | 'z'];
-  }
-
-  export type SVGPath = Array<
-    | SVGPath.M
-    | SVGPath.L
-    | SVGPath.H
-    | SVGPath.V
-    | SVGPath.C
-    | SVGPath.S
-    | SVGPath.Q
-    | SVGPath.T
-    | SVGPath.A
-    | SVGPath.Z
-  >;
-
   export type Label = string | { text: string; fill: Color };
 
   export type FitView =
