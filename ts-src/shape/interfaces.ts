ffcb6caa44ed754e9e57710d6935f87940e118b1
Harpes <823289099@qq.com>
2019-01-21 14:55:11 +0800

    完善四个register的类型声明

--- ts-src/shape/interfaces.ts
+++ ts-src/shape/interfaces.ts
@@ -1,15 +1,77 @@
+import G from '@antv/g/lib';
+import GShape from '@antv/g/lib/shapes';
+
 import '../../types';
+import { Common } from '../common';
+import Item from '../item';
+
 import _Shape from './shape';
 
-export type RegisterShape = <T>(name: string, cfg: T, extendShape?: string) => ShapeObject<T>;
+interface Node {
+  /** 绘制, 返回的图形既是该图项的 keyShape -- 关键形 */
+  draw?(item: Item.Node): InstanceType<GShape.Shape>;
+  /** 获取锚点 */
+  anchor?: Common.Points;
+  /** 默认样式 */
+  getStyle?(item: Item.Node): G.Style;
+  /** 选中样式 */
+  getSelectedStyle?(item: Item.Node): G.Style;
+}
+
+interface Edge {
+  /** 绘制, 返回的图形既是该图项的 keyShape -- 关键形 */
+  draw?(item: Item.Edge): InstanceType<GShape.Shape>;
+  /* 获取路径 */
+  getPath?(item: Item.Edge): Common.SVGPath;
+  /* 起始箭头 */
+  startArrow?: {
+    /* 路径 */
+    path?(item: Item.Edge): Common.SVGPath;
+    /* 线缩短偏移 */
+    shorten?(item: Item.Edge): number;
+    /* 样式 */
+    style?(item: Item.Edge): G.Style;
+  };
+  /* 结束箭头 */
+  endArrow?: {
+    /* 路径 */
+    path?(item: Item.Edge): Common.SVGPath;
+    /* 线缩短偏移 */
+    shorten?(item: Item.Edge): number;
+    /* 样式 */
+    style?(item: Item.Edge): G.Style;
+  };
+
+  /** 默认样式 */
+  getStyle?(item: Item.Edge): GShape.AttrsMap['path'];
+  /** 选中样式 */
+  getSelectedStyle?(item: Item.Edge): GShape.AttrsMap['path'];
+
+  afterDraw?(item: Item.Edge): void;
+}
+
+interface Group {
+  /** 绘制, 返回的图形既是该图项的 keyShape -- 关键形 */
+  draw?(item: Item.Group): InstanceType<GShape.Shape>;
+  /** 获取锚点 */
+  anchor?: Common.Points;
+}
+
+interface Guide {}
+
+export type RegisterShape<T extends Node | Edge | Group | Guide> = (
+  name: string,
+  cfg: T,
+  extendShape?: string
+) => void;
 
 export type Shape = {
   registerShapeManager<T>(type: string, cfg: T): ShapeManager<T>;
 
-  registerNode: RegisterShape;
-  registerEdge: RegisterShape;
-  registerGroup: RegisterShape;
-  registerGuide: RegisterShape;
+  registerNode: RegisterShape<Node>;
+  registerEdge: RegisterShape<Edge>;
+  registerGroup: RegisterShape<Group>;
+  registerGuide: RegisterShape<Guide>;
 
   Node: ShapeManager<{ defaultShapeType: 'common' }> & {
     common: typeof import('./nodes/common');
